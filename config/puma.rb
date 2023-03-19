6e9ea0aeadd79292768245f3ab428b45c0787228
Guotong <guotong@GuotongdeMacBook-Pro.local>
2023-03-19 18:50:13 -0400

    config puma config file path, port

--- config/puma.rb
+++ config/puma.rb
@@ -15,11 +15,13 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+port ENV.fetch("PORT") { 4000 }
 
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
+
+bind "unix:///home/ubuntu/apps/wiki.defi/current/tmp/sockets/puma.sock"
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
