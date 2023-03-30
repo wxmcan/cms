378928cd8cf2fc39e0165e303e87506eefdcacc7
Guotong <guotong@GuotongdeMacBook-Pro.local>
2023-03-30 14:22:58 -0400

    Generate sitemap url with HTTPS

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://wiki.defi.io"
+SitemapGenerator::Sitemap.default_host = "https://wiki.defi.io"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
