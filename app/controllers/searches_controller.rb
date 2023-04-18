667569fd9953547991ffb9297876b7e65faf5344
Guotong <guotong@GuotongdeMacBook-Pro.local>
2023-04-18 19:27:19 -0400

    Add full-text search function

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,7 +5,7 @@ class SearchesController < ApplicationController
 
   def create
     q = params[:q].gsub(/\s+/, "")
-    @results = Spina::Page.live.where("jsonb_extract_path_text(json_attributes, 'id_content') ILIKE ? OR jsonb_extract_path_text(json_attributes, 'zh-CN_content') ILIKE ?", "%#{q}%", "%#{q}%")
+    @results = Spina::Page.live.where("jsonb_extract_path_text(json_attributes, 'en_content') ILIKE ? OR jsonb_extract_path_text(json_attributes, 'zh-CN_content') ILIKE ?", "%#{q}%", "%#{q}%")
 
     respond_to do |format|
       format.turbo_stream { render turbo_stream: turbo_stream.replace('searches', partial: 'searches/search') }
